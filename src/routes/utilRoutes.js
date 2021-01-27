89e94436bc27fc2e3c527a82b9aaa503d2a06b1b
Erivan <erivan.txtbrasil@gmail.com>
2021-01-26 22:48:19 -0300

    v1

--- src/routes/utilRoutes.js
+++ src/routes/utilRoutes.js
@@ -7,6 +7,9 @@ class UtilRoutes extends BaseRoute {
             path: '/coverage/{param*}',
             method: 'GET',
             config: {
+                tags: ['api'],
+                description: 'Cobertura de código',
+                notes: ' a quantidade do código!',
                 auth: false
             },
             handler:{
